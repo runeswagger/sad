13b35a4964a2c82958f4146e02ae587fc916a6d0
Timothy Loiselle <timothy3592@gmail.com>
2015-11-13 05:20:15 -0500

    this is the test program for event.c

---
+++ event-test.c
@@ -0,0 +1,24 @@
+/* print out events */
+/* a test for expanding the event library */
+#include "event.h"
+#include "delay.h"
+
+int main(int argc, char *argv[]){
+	//poll for and print events
+	int delay = argc > 1 ? atoi(&argv[1][2]) : 1000/60; //60fps
+	struct input_event event;
+	int running = 1;
+	
+	addeventsource("/dev/input/event0");
+	addeventsource("/dev/input/event12");
+
+	while(running){
+		while(pollevent(&event)){
+			printf("Type: %i\nCode: %i\nValue: %i\n\n", event.type, event.code, event.value);
+			if(event.type == EV_KEY && event.value == 1 && event.code == KEY_Q) running = 0; //quit program
+			_delay_ms(delay);
+		}
+	}
+	
+	return 0;
+}
